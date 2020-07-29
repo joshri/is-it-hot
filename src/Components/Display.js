424860714128225ef5deca7b871ce3a2cc3ff125
Joshua Israel <joshisrael93@gmail.com>
2020-07-29 13:32:04 -0400

    changed display to react hooks

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,100 +1,83 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const weatherKey = process.env.REACT_APP_WEATHER_KEY;
-let date = new Date();
-date.toUTCString();
+function Display(props) {
+	const weatherKey = process.env.REACT_APP_WEATHER_KEY;
 
-class Display extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			weather: {
-				name: '?',
-                feels_like: '?',
-                skys: '?',
-			},
-			hotOrNot: '?',
-            advice: '?',
-            retro: '?',
-		};
-	}
+	let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${props.zip}&units=imperial&appid=${weatherKey}`;
 
-	setWeather = (json) => {
-		this.setState({
-			weather: {
-				name: json.name,
-                feels_like: json.main.feels_like,
-                skys: json.weather[0].main,
-			},
-		});
-	};
+	let date = new Date();
+	date.toUTCString();
 
-	setAdvice = (advice) => {
-		this.setState({ advice: advice });
-    };
-    
-    setRetro = (json) => {
-        if (json.is_retrograde) {
-        this.setState({ retro: 'Good news! Mercury is in retrograde'})
-        } 
-        this.setState( {retro: 'Unfortunately, Mercury is not in retrograde...'} )
-    }
+	let [weather, setWeather] = useState({
+		name: '',
+		feels_like: '',
+		skies: '',
+	});
+	let [hotOrNot, setHotOrNot] = useState('');
+	let [advice, setAdvice] = useState('');
+	let [retro, setRetro] = useState('');
+	let [dog, setDog] = useState('');
 
-	hotOrNot = (weather) => {
-		if (weather.feels_like < 65) {
-			return this.setState({ hotOrNot: 'IT\'S NOT' });
-		}
-		return this.setState({ hotOrNot: 'IT\'S HOT' });
-	};
-
-	componentDidMount() {
-		let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${this.props.zip}&units=imperial&appid=${weatherKey}`;
+	useEffect(() => {
 		fetch(weatherUrl)
 			.then((res) => res.json())
 			.then((json) => {
-                console.log(json);
-                 this.setWeather(json);
-                 this.hotOrNot(json.main);
+				setWeather({
+					name: json.name,
+					feels_like: json.main.feels_like,
+					skies: json.weather[0].main,
+				});
+				setHotOrNot(json.main.feels_like < 65 ? 'NOT' : 'HOT');
 			})
-			.catch(() => {
-				this.props.showError();
-			});
+			.catch(() => props.showError());
 		fetch('https://api.adviceslip.com/advice')
 			.then((res) => res.json())
+			.then((json) => setAdvice(json.slip.advice));
+		fetch(`https://mercuryretrogradeapi.com?date=${date}`)
+			.then((res) => res.json())
 			.then((json) => {
-                this.setAdvice(json.slip.advice)
-            });
-        fetch(`https://mercuryretrogradeapi.com?date=${date}`)
-        .then(res => res.json())
-        .then(json => {
-            this.setRetro(json)
-        })
-	}
+				if (json.is_retrograde) {
+					setRetro('Good news! Mercury is in retrograde! You can stay home.');
+				}
+				setRetro(
+					'Unfortunately, Mercury is not in retrograde...looks like you have to go outside.'
+				);
+			});
+		fetch('https://dog.ceo/api/breeds/image/random')
+			.then((res) => res.json())
+			.then((json) => {
+				setDog(json.message);
+			});
+	}, []);
 
-	render() {
-		return (
+	return (
+		<div>
 			<main>
 				<div className='weather'>
-					<h1>{this.state.hotOrNot}</h1>
+					<h1>{hotOrNot}</h1>
 					<h2>
-						Feels exactly like {this.state.weather.feels_like} degrees in good
-						ol' {this.state.weather.name}
+						Feels exactly like {weather.feels_like} degrees in good ol'
+						{weather.name}
 					</h2>
-                    <h3>Is it raining?</h3>
-                    <h2>WHO CARES?</h2>
-                    <h4>jk - SKYS: {this.state.weather.skys}</h4>
+					<h3>Is it wet?</h3>
+					<h2>WHO CARES?</h2>
+					<h4>jk - skies: {weather.skies}</h4>
 				</div>
 				<div className='advice'>
-                    <h2>You know what they say...</h2>
-                    <h3>"{this.state.advice}"</h3>
-                </div>
-                <div className='retro'>
-                    <h2>Need an excuse?</h2>
-                    <h3>{this.state.retro}</h3>
-                </div>
+					<h2>You know what they say...</h2>
+					<h3>"{advice}"</h3>
+				</div>
+				<div className='retro'>
+					<h2>Need an excuse?</h2>
+					<h3>{retro}</h3>
+				</div>
+				<div className='animals'>
+					<h2>Here is a dog:</h2>
+					<img className='dog' src={dog} />
+				</div>
 			</main>
-		);
-	}
+		</div>
+	);
 }
 
 export default Display;
